7c76a4fa35c9e990a78841c4b692773ca42275d5
ksushacoker <coker0464@gmail.com>
2024-09-29 20:20:14 +0300

    Add files via upload

---
+++ p.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PUSSY POWER</title>
+    <style>
+      p{
+        font-family: Cambria, Cochin, Georgia, Times, 'Times New Roman', serif;
+      }
+    </style>
+</head>
+<center>
+<body style= "background-color:#1E201E;color:#ECDFCC;margin-top:100px"> 
+<img src="pp.jpg" width="220" height="220" style= "border-radius:20px;background-size:cover">
+
+<p>PUSSY POWER - INFECTION 0.5 (DEMO)</p>
+<br>
+   
+    <audio controls style="border:none;border-radius:20px">
+      <source src="dark.mp3" type="audio/mpeg">
+     sorry , i'm really sorry
+    </audio>
+    
+</body>
+</center>
+
+</html>
